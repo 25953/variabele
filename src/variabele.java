427e7a8ce52c2500abf203321307383d4e41ea26
Kevin Kranenburg <kevinkranenburg@MacBook-Pro-van-Kevin.local>
2018-09-28 14:39:49 +0200

    Initial commit

---
+++ src/variabele.java
@@ -0,0 +1,14 @@
+public class variabele {
+
+    public static void main (string[] args ) {
+
+        Boolean ja = true;
+        String naam = "tekst";
+
+        System.out.println ("eyo boys");
+        System.out.println (ja);
+        System.out.println (naam + naam);
+
+
+    }
+}
